3d5d212a8d4218f12bc27ed484e6a15bb9d296a5
KhMansur <mxamrayev888@gmail.com>
2022-12-17 21:28:38 +0500

    BROKERS: Insert Ticket

---
+++ MockInterview.Api/Brokers/Storages/IStorageBroker.Ticket.cs
@@ -0,0 +1,10 @@
+﻿using MockInterview.Api.Models.Tickets;
+using System.Threading.Tasks;
+
+namespace MockInterview.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Ticket> InsertTicketAsync(Ticket ticket);
+    }
+}
--- MockInterview.Api/Brokers/Storages/StorageBroker.Ticket.cs
+++ MockInterview.Api/Brokers/Storages/StorageBroker.Ticket.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MockInterview.Api.Models.Tickets;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace MockInterview.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         DbSet<Ticket> Tickets { get; set; }
+
+        public async ValueTask<Ticket> InsertPostAsync(Ticket ticket) =>
+            await InsertAsync(ticket);
     }
 }
