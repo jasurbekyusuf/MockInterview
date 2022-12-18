d483d05e643f24f5efe57b0c9bb1cfcfae2f1a00
MrOOI <ogabekochilov25@gmail.com>
2022-12-18 09:28:20 -0800

    CODE RUB: LoggingBroker Add

--- MockInterview.Api/Brokers/Loggings/LoggingBroker.cs
+++ MockInterview.Api/Brokers/Loggings/LoggingBroker.cs
@@ -1,6 +1,18 @@
-﻿namespace MockInterview.Api.Brokers.Loggings
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace MockInterview.Api.Brokers.Loggings
 {
-    public class LoggingBroker
+    public class LoggingBroker : ILoggingBroker
     {
+        private ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception.Message, exception);
+
+        public void LogCritical(Exception exception) =>
+            this.logger.LogCritical(exception, exception.Message);
     }
 }
