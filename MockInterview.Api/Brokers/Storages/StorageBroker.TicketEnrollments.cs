7720df180498720a97e343cb99fbd8a92be455bf
KhMansur <mxamrayev888@gmail.com>
2022-12-18 08:26:03 +0500

    BROKERS: Insert TicketEnrollment

--- MockInterview.Api/Brokers/Storages/StorageBroker.TicketEnrollments.cs
+++ MockInterview.Api/Brokers/Storages/StorageBroker.TicketEnrollments.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MockInterview.Api.Models.TicketEnrollments;
 using MockInterview.Api.Models.Tickets;
+using System.Threading.Tasks;
 
 namespace MockInterview.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         DbSet<TicketEnrollment> TicketEnrollments { get; set; }
+
+        public async ValueTask<TicketEnrollment> InsertTicketEnrollmentAsync(TicketEnrollment ticketEnrollment) =>
+            await InsertAsync(ticketEnrollment);
     }
 }
