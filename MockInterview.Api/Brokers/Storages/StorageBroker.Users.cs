cea67e8ceba49276c40d02ce6c9a8f69175e0674
Jasubek yusufov <jasurbekyusufov@gmail.com>
2022-12-17 19:45:17 -0800

    BROKERS: Update User

--- MockInterview.Api/Brokers/Storages/StorageBroker.Users.cs
+++ MockInterview.Api/Brokers/Storages/StorageBroker.Users.cs
@@ -3,6 +3,7 @@
 // Free to use to bring order in your workplace
 //==================================================
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MockInterview.Api.Models.Users;
 
@@ -11,5 +12,11 @@ namespace MockInterview.Api.Brokers.Storages
     public partial class StorageBroker
     {
         DbSet<User> Users { get; set; }
+
+        public async ValueTask<User> InsertUserAsync(User user) =>
+            await InsertAsync(user);
+
+        public async ValueTask<User> UpdateUserAsync(User user) =>
+            await UpdateAsync(user);
     }
 }
