2355dee99fcfe6619dd33297e009e6a22d262dac
MrOOI <ogabekochilov25@gmail.com>
2022-12-17 22:08:11 -0800

    DATA: Model And Migrate Users

---
+++ MockInterview.Api/Models/Users/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MockInterview.Api.Models.Users
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Role { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string PasswordHash { get; set; }
+    }
+}
